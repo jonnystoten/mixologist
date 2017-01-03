adf80d934c5833d1688e17cd905c1311137f5c06
Jonny Stoten <jonny@rocketmakers.com>
2017-01-03 16:21:30 +0000

    Refactor loading

--- stir/loading.go
+++ stir/loading.go
@@ -5,27 +5,22 @@ import "jonnystoten.com/mixologist/mix"
 type LoadOp struct{ mix.Instruction }
 
 func (op LoadOp) Execute(c *Computer) {
+	word := mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
+
+	if mix.LDAN <= op.OpCode && op.OpCode <= mix.LDXN {
+		word = mix.ToggleSign(word)
+	}
+
 	switch {
-	case op.OpCode == mix.LDA:
-		c.Accumulator = mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
+	case op.OpCode == mix.LDA || op.OpCode == mix.LDAN:
+		c.Accumulator = word
+	case op.OpCode == mix.LDX || op.OpCode == mix.LDXN:
+		c.Extension = word
 	case mix.LD1 <= op.OpCode && op.OpCode <= mix.LD6:
 		index := op.OpCode - mix.LD1
-		word := mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
 		c.Index[index] = mix.CastAsAddress(word)
-	case op.OpCode == mix.LDX:
-		c.Extension = mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
-	case op.OpCode == mix.LDAN:
-		word := mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
-		word = mix.ToggleSign(word)
-		c.Accumulator = word
 	case mix.LD1N <= op.OpCode && op.OpCode <= mix.LD6N:
 		index := op.OpCode - mix.LD1N
-		word := mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
-		word = mix.ToggleSign(word)
 		c.Index[index] = mix.CastAsAddress(word)
-	case op.OpCode == mix.LDXN:
-		word := mix.ApplyFieldSpec(c.Memory[c.getIndexedAddressValue(op.Instruction)], op.FieldSpec)
-		word = mix.ToggleSign(word)
-		c.Extension = word
 	}
 }
